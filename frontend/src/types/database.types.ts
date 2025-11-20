6b8b37ff03244dac8d5a2833ee4d3546efe85683
Nithish S <nithish952001@gmail.com>
2025-11-20 14:22:56 -0800

    feat: Add portfolio operations and database migration documentation

---
+++ frontend/src/types/database.types.ts
@@ -0,0 +1,339 @@
+export type Json =
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json | undefined }
+    | Json[]
+
+export type Database = {
+    // Allows to automatically instantiate createClient with right options
+    // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+    __InternalSupabase: {
+        PostgrestVersion: "13.0.5"
+    }
+    public: {
+        Tables: {
+            contact_submissions: {
+                Row: {
+                    created_at: string | null
+                    email: string | null
+                    id: number
+                    message: string | null
+                    name: string | null
+                    phone: string | null
+                    sport: string | null
+                    topic: string | null
+                }
+                Insert: {
+                    created_at?: string | null
+                    email?: string | null
+                    id?: number
+                    message?: string | null
+                    name?: string | null
+                    phone?: string | null
+                    sport?: string | null
+                    topic?: string | null
+                }
+                Update: {
+                    created_at?: string | null
+                    email?: string | null
+                    id?: number
+                    message?: string | null
+                    name?: string | null
+                    phone?: string | null
+                    sport?: string | null
+                    topic?: string | null
+                }
+                Relationships: []
+            }
+            files: {
+                Row: {
+                    created_at: string
+                    file_url: string | null
+                    id: number
+                    name: string | null
+                    user_id: string | null
+                }
+                Insert: {
+                    created_at?: string
+                    file_url?: string | null
+                    id?: never
+                    name?: string | null
+                    user_id?: string | null
+                }
+                Update: {
+                    created_at?: string
+                    file_url?: string | null
+                    id?: never
+                    name?: string | null
+                    user_id?: string | null
+                }
+                Relationships: []
+            }
+            portfolios: {
+                Row: {
+                    achievements: Json | null
+                    action_photos_urls: Json | null
+                    background: string | null
+                    bio: string | null
+                    certifications_url: string | null
+                    comments: string | null
+                    contact_email: string
+                    created_at: string | null
+                    educational_background: Json | null
+                    endorsements: string | null
+                    experience: Json | null
+                    first_name: string
+                    goals: string | null
+                    has_logo: boolean | null
+                    id: string
+                    last_name: string
+                    logo_url: string | null
+                    media_links: Json | null
+                    merch: string | null
+                    middle_name: string | null
+                    more_media_urls: Json | null
+                    opportunities: string | null
+                    phone: string | null
+                    press: Json | null
+                    profile_picture_url: string | null
+                    references_url: string | null
+                    resume_url: string | null
+                    skills: Json | null
+                    socials: Json | null
+                    sport: string
+                    timeline: Json | null
+                    user_id: string | null
+                    video_links: Json | null
+                }
+                Insert: {
+                    achievements?: Json | null
+                    action_photos_urls?: Json | null
+                    background?: string | null
+                    bio?: string | null
+                    certifications_url?: string | null
+                    comments?: string | null
+                    contact_email: string
+                    created_at?: string | null
+                    educational_background?: Json | null
+                    endorsements?: string | null
+                    experience?: Json | null
+                    first_name: string
+                    goals?: string | null
+                    has_logo?: boolean | null
+                    id?: string
+                    last_name: string
+                    logo_url?: string | null
+                    media_links?: Json | null
+                    merch?: string | null
+                    middle_name?: string | null
+                    more_media_urls?: Json | null
+                    opportunities?: string | null
+                    phone?: string | null
+                    press?: Json | null
+                    profile_picture_url?: string | null
+                    references_url?: string | null
+                    resume_url?: string | null
+                    skills?: Json | null
+                    socials?: Json | null
+                    sport: string
+                    timeline?: Json | null
+                    user_id?: string | null
+                    video_links?: Json | null
+                }
+                Update: {
+                    achievements?: Json | null
+                    action_photos_urls?: Json | null
+                    background?: string | null
+                    bio?: string | null
+                    certifications_url?: string | null
+                    comments?: string | null
+                    contact_email?: string
+                    created_at?: string | null
+                    educational_background?: Json | null
+                    endorsements?: string | null
+                    experience?: Json | null
+                    first_name?: string
+                    goals?: string | null
+                    has_logo?: boolean | null
+                    id?: string
+                    last_name?: string
+                    logo_url?: string | null
+                    media_links?: Json | null
+                    merch?: string | null
+                    middle_name?: string | null
+                    more_media_urls?: Json | null
+                    opportunities?: string | null
+                    phone?: string | null
+                    press?: Json | null
+                    profile_picture_url?: string | null
+                    references_url?: string | null
+                    resume_url?: string | null
+                    skills?: Json | null
+                    socials?: Json | null
+                    sport?: string
+                    timeline?: Json | null
+                    user_id?: string | null
+                    video_links?: Json | null
+                }
+                Relationships: []
+            }
+            profiles: {
+                Row: {
+                    avatar_url: string | null
+                    id: string
+                    updated_at: string | null
+                    username: string | null
+                }
+                Insert: {
+                    avatar_url?: string | null
+                    id: string
+                    updated_at?: string | null
+                    username?: string | null
+                }
+                Update: {
+                    avatar_url?: string | null
+                    id?: string
+                    updated_at?: string | null
+                    username?: string | null
+                }
+                Relationships: []
+            }
+        }
+        Views: {
+            [_ in never]: never
+        }
+        Functions: {
+            [_ in never]: never
+        }
+        Enums: {
+            [_ in never]: never
+        }
+        CompositeTypes: {
+            [_ in never]: never
+        }
+    }
+}
+
+type DefaultSchema = Database["public"]
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+export type Tables<
+    DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+        schema: keyof DatabaseWithoutInternals
+    }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+}
+    ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+            Row: infer R
+        }
+    ? R
+    : never
+    : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+            Row: infer R
+        }
+    ? R
+    : never
+    : never
+
+export type TablesInsert<
+    DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+        schema: keyof DatabaseWithoutInternals
+    }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+}
+    ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+        Insert: infer I
+    }
+    ? I
+    : never
+    : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+    }
+    ? I
+    : never
+    : never
+
+export type TablesUpdate<
+    DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+    TableName extends DefaultSchemaTableNameOrOptions extends {
+        schema: keyof DatabaseWithoutInternals
+    }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+}
+    ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+        Update: infer U
+    }
+    ? U
+    : never
+    : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+    }
+    ? U
+    : never
+    : never
+
+export type Enums<
+    DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof DatabaseWithoutInternals },
+    EnumName extends DefaultSchemaEnumNameOrOptions extends {
+        schema: keyof DatabaseWithoutInternals
+    }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+}
+    ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+    : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+    PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+    CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+        schema: keyof DatabaseWithoutInternals
+    }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+}
+    ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+    : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+    public: {
+        Enums: {},
+    },
+} as const
